c6392523af516e4e5190788a7eb619e2be3552b2
Rex Daisey <rexdaisey@gmail.com>
2022-10-11 20:39:51 -0400

    Update streamlit_app.py
    added emoji's

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,8 @@ streamlit.header('Header One 1')
 streamlit.text('Text aaAAA')
 streamlit.text('Text  bbBBBB')
 streamlit.text('Text cc CCCC')
+
+
+
+
+streamlit.header('\N{flexed biceps} Champion reference here \N{flexed biceps}')
