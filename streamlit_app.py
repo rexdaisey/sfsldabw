77c6086468ca47454944e45d2f12460d1b3d4f49
Rex Daisey <rexdaisey@gmail.com>
2022-10-11 20:38:07 -0400

    Update streamlit_app.py
    replaced header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 
+streamlit.title('Title Line A One')
+
 streamlit.header('Header One 1')
+
 streamlit.text('Text aaAAA')
 streamlit.text('Text  bbBBBB')
 streamlit.text('Text cc CCCC')
