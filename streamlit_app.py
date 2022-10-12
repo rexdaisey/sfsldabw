976266c8efbb5272dd955bcda87605690969bb69
Rex Daisey <rexdaisey@gmail.com>
2022-10-11 21:12:57 -0400

    Update streamlit_app.py
    set_index

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.text('Text cc CCCC')
 streamlit.header('\N{flexed biceps} Champion reference here \N{flexed biceps}')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # make picklist
 streamlit.multiselect("select item here:", list(my_fruit_list.index))
