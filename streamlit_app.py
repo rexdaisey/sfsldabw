e1f9dfc9698524f60031f6b83330c8ef07d9566e
Rex Daisey <rexdaisey@gmail.com>
2022-10-11 20:37:17 -0400

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
-streamlit.title('sample title dddd')
+streamlit.header('Header One 1')
+streamlit.text('Text aaAAA')
+streamlit.text('Text  bbBBBB')
+streamlit.text('Text cc CCCC')
