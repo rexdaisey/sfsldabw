9628cc11689a0f9eb16b70b875891b1b26028df1
Rex Daisey <rexdaisey@gmail.com>
2022-10-14 10:08:25 -0400

    Update streamlit_app.py
    noted error message

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 # streamlit.header('Full List')
 # streamlit.dataframe(my_fruit_list)
 
-fruit_choice = streamlit.text_input('enter a fruit?','Kiwi')
+fruit_choice = streamlit.text_input('enter a fruit?','Kiwi')  # make a default to avoid an error message
 streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
