5922a62d81148b483ec82ccec86a617c60e4827c
Rex Daisey <rexdaisey@gmail.com>
2022-10-14 09:57:07 -0400

    Update streamlit_app.py
    json normalized

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,8 @@ streamlit.dataframe(fruits_to_show)
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# streamlit.text(fruityvice_response)
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response) # response code 200
+streamlit.text(fruityvice_response.json()) # writes raw json to screen
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
 
