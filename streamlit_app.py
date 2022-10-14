459c0fc826586d1355f69b645f754bdf112061cc
Rex Daisey <rexdaisey@gmail.com>
2022-10-14 09:54:09 -0400

    Update streamlit_app.py
    added .json() to response

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,6 @@ streamlit.dataframe(fruits_to_show)
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+# streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
