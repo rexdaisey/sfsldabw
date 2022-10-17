29ba3e46de2b8603b40a461bcd6463865c5b8769
Rex Daisey <rexdaisey@gmail.com>
2022-10-17 10:01:58 -0400

    Update streamlit_app.py
    added Load List Query and Load into a Button Action

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,14 +48,24 @@ except URLError as e:
     streamlit.error()
 
 streamlit.header(' begin snowflake connect ')
-    
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+# snowflake related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * from fruit_load_list")
+        return my_cur.fetchall()
+
+# add a button to load the data
+if streamlit.button('Get f Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    # my_data_rows = my_cur.fetchall()
+    my_data_rows = get_fruit_load_list()
+    streamlit.header("test data retrned from Snowflake:")
+    streamlit.dataframe(my_data_rows)    
+    # my_cur = my_cnx.cursor()
+
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("test data retrned from Snowflake:")
-streamlit.dataframe(my_data_rows)
+
+streamlit.header(" begin data to add section ")
 
 record_to_add = streamlit.text_input('enter a record to add','jackfruit')  # make a default to avoid an error message
 streamlit.write('recrod to add here: ', record_to_add)
